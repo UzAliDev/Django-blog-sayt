df9147ca3417027a2dfd35fcb4c24ee606046c3a
UzAliDev <abduvali852369@gmail.com>
2023-01-15 21:38:27 +0500

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your views here.
+
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
